b89b4f7adae367325ee06324388adc7fc792b9aa
root <root@client2.demo.com>
2018-12-31 15:50:18 +0900

    Update Script

--- script/prerequisites_install_script.sh
+++ script/prerequisites_install_script.sh
@@ -11,7 +11,6 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-
 # Check SElinux
 echo -e "${RED} * * * * * Check Selinux * * * * * ${NC}"
 
@@ -21,29 +20,44 @@ if [ "$selinux" -ne 1 ]
 	then
 	sed -i "s/^SELINUX=.*$/SELINUX=enforcing/" /etc/sysconfig/selinux
         echo -e "${RED}SElinux Change, Please System Reboot${NC}"
+	sleep 2
+	exit 0
 fi
+echo " "
+sleep 1
 
 # Firewalld disable
 echo -e "${RED} * * * * * Check Firewalld * * * * * ${NC}"
 systemctl disable firewalld
 systemctl stop firewalld
+echo " "
 
 # NetworkManager Check
 echo -e "${RED} * * * * * Check NetworkManager * * * * * ${NC}"
 systemctl enable NetworkManager
 systemctl start NetworkManager
+sleep 1
 
 # preinstall yum package 
+echo " "
 
 echo -e "${RED} * * * * * Preinstall Package * * * * * ${NC}"
-yum -y install bash-com* wget git yum-utils createrepo epel-re* centos-release-openshift-origin39.noarch
+yum -y install bash-completion wget git yum-utils createrepo epel-release centos-release-openshift-origin39.noarch
 yum clean all;yum repolist
+sleep 1
+echo " "
+echo " "
 yum -y install ansible docker
+echo " "
+echo " "
+sleep 2
 
 # docker enable
 echo -e "${RED} * * * * * start docker service * * * * * ${NC}"
 systemctl enable docker
 systemctl start docker
+echo " "
+sleep 1
 
 # git clone
 echo -e "${RED}openshift-ansible clone ${NC}"
@@ -51,5 +65,5 @@ cd ~
 git clone https://github.com/openshift/openshift-ansible
 cd openshift-ansible/
 git checkout release-3.9
-
+echo " "
 echo -e "${RED}= = = = = SCRIPT END = = = = = ${NC}"
